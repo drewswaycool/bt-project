bb728d7a462c2efd04c3851390ea8c49775eee17
Drew McMurry <andrewsmcmurry@familysearch.org>
2022-06-18 15:59:57 -0600

    fix auth error

--- src/components/CardError.js
+++ src/components/CardError.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CardError({error}) {
   return (
     <div className="card-error">
-        <h3>Error: {error.message}</h3>
+        <h3>Error: {error.message || error}</h3>
     </div>
   )
 }
